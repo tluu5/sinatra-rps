25b85d56d389f9218f7d3f7f984f8678a8f385e9
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2023-12-28 00:54:56 +0000

    first commit working rock

--- app.rb
+++ app.rb
@@ -7,3 +7,27 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won" 
+  end
+  
+  erb(:zebra)
+end
+
+get("/paper") do 
+  "This section is for user playing paper"
+end
+
+get("/scissors") do
+  "This section is for user playing scissors"
+end
