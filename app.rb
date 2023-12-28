ae1e26292421e70bc322abbdf55a1deefa23932b
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2023-12-28 05:02:18 +0000

    commit working game

--- app.rb
+++ app.rb
@@ -2,10 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rules)
 end
 
 get("/rock") do
@@ -21,13 +18,37 @@ get("/rock") do
     @outcome = "won" 
   end
   
-  erb(:zebra)
+  erb(:rock)
 end
 
 get("/paper") do 
-  "This section is for user playing paper"
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "won"
+  elsif @comp_move == "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
+
+  erb(:paper)
 end
 
 get("/scissors") do
-  "This section is for user playing scissors"
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+
+  erb(:scissors)
 end
